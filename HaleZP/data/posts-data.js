53ae177ef27b373307f1e115eda57cf551f8e831
ProH <1063869887wdh@gmail.com>
2017-07-13 00:31:15 +0800

    WeiChatApp
    WeiChatApp

---
+++ HaleZP/data/posts-data.js
@@ -0,0 +1,121 @@
+var local_database = [
+    {
+        date: "Jul 26 2017",
+        title: "京东首页",
+        imgSrc: "/images/post/crab.png",
+        avatar: "/images/avatar/7.png",
+        content: "京东首页，各种布局，居中，排版，对其",
+        reading: "112",
+        collection: "96",
+        headImgSrc: "/images/post/crab.png",
+        author: "王东晖",
+        dateTime: "24小时前",
+        detail: "用CSS和HTML搭建的京东首页用CSS和HTML搭建的京东首页用CSS和HTML搭建的京东首页用CSS和HTML搭建的京东首页用CSS和HTML搭建的京东首页用CSS和HTML搭建的京东首页用CSS和HTML搭建的京东首页用CSS和HTML搭建的京东首页用CSS和HTML搭建的京东首页用CSS和HTML搭建的京东首页用CSS和HTML搭建的京东首页用CSS和HTML搭建的京东首页用CSS和HTML搭建的京东首页用CSS和HTML搭建的京东首页用CSS和HTML搭建的京东首页用CSS和HTML搭建的京东首页",
+        postId: 0,
+        music: {
+            url: "http://ws.stream.qqmusic.qq.com/C100003507bR0gDKBm.m4a?fromtag=38",
+            title: "夜夜夜夜-齐秦",
+            coverImg: "http://y.gtimg.cn/music/photo_new/T002R150x150M000001TEc6V0kjpVC.jpg?max_age=2592000"
+        }
+    },
+    {
+      title: "H5+CSS3+JS实现炫酷网页",
+        content: "滑动门特效，导航栏数据的双向绑定，滚动加载，css雪碧图，呼吸灯的动态特效，PxCook的使用...",
+        imgSrc: "/images/post/bl.png",
+        reading: 62,
+        detail: "滑动门特效，导航栏数据的双向绑定，滚动加载，css雪碧图，呼吸灯的动态特效，PxCook（像素大厨应用）的使用;滑动门特效，导航栏数据的双向绑定，滚动加载，css雪碧图，呼吸灯的动态特效，PxCook（像素大厨应用）的使用;滑动门特效，导航栏数据的双向绑定，滚动加载，css雪碧图，呼吸灯的动态特效，PxCook（像素大厨应用）的使用滑动门特效，导航栏数据的双向绑定，滚动加载，css雪碧图，呼吸灯的动态特效，PxCook（像素大厨应用）的使用",
+        collection: 92,
+        dateTime: "24小时前",
+        headImgSrc: "/images/post/bl.png",
+        author: "王东晖",
+        date: "Jul  25  2017",
+        avatar: "/images/avatar/7.png",
+        postId: 1,
+        music: {
+            url: "http://ws.stream.qqmusic.qq.com/C100003GdCmG4NkEOR.m4a?fromtag=38",
+            title: "鬼迷心窍-李宗盛",
+            coverImg: "http://y.gtimg.cn/music/photo_new/T002R150x150M000002xOmp62kqSic.jpg?max_age=2592000"
+        }
+    },
+    {
+        //按住alt + shift + F 可以格式化代码样式
+      title: "Github+Hexo搭建了个人独立博客：proh.online",
+      content: "用Github+Hexo搭建了个人独立博客：proh.online；使用了Next主题...",
+      detail: "用Github+Hexo搭建了个人独立博客：proh.online；使用了Next主题，简洁，强大；采用MarkDown编排；如你所见，有域名ProH.onLine；站内可以搜索、评论、分享等（PC端体验更酷）；有音乐模块，阅读统计模块，评论模块，站内搜索模块，标签模块，导航栏模块，分享模块，个人信息模块，热评模块等",
+        imgSrc: "/images/post/sls.jpg",
+        headImgSrc: "/images/post/sls.jpg",
+        reading: 62,
+        collection: 92,
+        author: "王东晖",
+        date: "Jul 24 2017",
+        dateTime: "三天前",
+        avatar: "/images/avatar/7.png",
+        postId: 2,
+        music: {
+            url: "http://ws.stream.qqmusic.qq.com/C100004HLusI2lLjZy.m4a?fromtag=38",
+            title: "女儿情-万晓利",
+            coverImg: "http://y.gtimg.cn/music/photo_new/T002R150x150M000004Wv5BO30pPc0.jpg?max_age=2592000"
+        }
+    },
+    {
+        title: "书城整站WebApp",
+        content: "用Koa、Vue.js、Zepto等框架结合HTML5开发一个完整的包含有阅读器功能的书城App整站...",
+        imgSrc: "/images/post/xiaolong.jpg",
+        reading: 102,
+        detail: "用Koa、Vue.js、Zepto等框架结合HTML5开发一个完整的包含有阅读器功能的书城App整站。从需求分析，设计图解析，技术选型，环境搭建直到开发测试和细节优化，整个过程中有几个比较复杂的几个模块，如阅读器，书城首页，搜索模块等",
+        collection: 92,
+        dateTime: "24小时前",
+        headImgSrc: "/images/post/xiaolong.jpg",
+        author: "王东晖",
+        date: "Jul 21 2017",
+        avatar: "/images/avatar/7.png",
+        postId: 3,
+        music: {
+            url: "http://ws.stream.qqmusic.qq.com/C100002mWVx72p8Ugp.m4a?fromtag=38",
+            title: "恋恋风尘-老狼",
+            coverImg: "http://y.gtimg.cn/music/photo_new/T002R150x150M000001VaXQX1Z1Imq.jpg?max_age=2592000",
+        }
+    },
+    {
+        title: "LovingFish小游戏",
+        content: "在蓝色海里，有飘动的海葵和漂浮物,大鱼妈妈和小鱼;海葵不断产生蓝色和橘色果实；大鱼要及时吃果实去喂小鱼，否则小鱼会饿死，吃到橘色果实分数加倍。",
+        imgSrc: "/images/post/vr.png",
+        reading: 102,
+        detail: "在蓝色海里，有飘动的海葵和漂浮物,大鱼妈妈和小鱼;海葵不断产生蓝色和橘色果实；大鱼要及时吃果实去喂小鱼，否则小鱼会饿死，吃到橘色果实分数加倍。Canvas大部分API;以及两点之间距离，趋近于等概念;浏览器提供的动画帧函数requestAnimFrame();池的概念;Math.sin()，Math.atan2()，Math.random()等数学函数",
+        collection: 26,
+        dateTime: "24小时前",
+        headImgSrc: "/images/post/vr.png",
+        author: "王东晖",
+        date: "Jul 23 2017",
+        avatar: "../../../images/avatar/7.png",
+        postId: 4,
+        music: {
+            url: "http://ws.stream.qqmusic.qq.com/C100000Zn0vS4fKKo8.m4a?fromtag=38",
+            title: "沉默是金-张国荣",
+            coverImg: "http://y.gtimg.cn/music/photo_new/T002R150x150M000003at0mJ2YrR2H.jpg?max_age=2592000"
+        }
+    },
+    {
+        title: "组件方式 WebApp 动态数据报告",
+        content: "运用HTML5、CSS3、JS流行技术，采用组件式开发模式，开发的Web App全站，各种不同类型的HTML5动态数据报告，有雷达图，折线图，散点图，饼图，条形图等...",
+        imgSrc: "/images/post/cat.png",
+        reading: 96,
+        detail: "运用HTML5、CSS3、JS流行技术，采用组件式开发模式，开发的Web App全站，各种不同类型的HTML5动态数据报告，有雷达图，折线图，散点图，饼图，条形图等。对数据可视化有更深的理解，用参数的从0到1的变化来控制动画的生长;更熟练的运用组件式开发思想",
+        collection: 26,
+        dateTime: "21小时前",
+        headImgSrc: "/images/post/cat.png",
+        author: "王东晖",
+        date: "Jul 20 2017",
+        avatar: "../../../images/avatar/7.png",
+        postId: 5,
+        music: {
+            url: "http://ws.stream.qqmusic.qq.com/C100002I8eGJ28BI17.m4a?fromtag=38",
+            title: "朋友-谭咏麟",
+            coverImg: "http://y.gtimg.cn/music/photo_new/T002R150x150M000004eGsCN3SUheO.jpg?max_age=2592000"
+        }
+    },
+]
+
+module.exports = {
+    postList: local_database
+}
